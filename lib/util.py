0bf9bf8899d041d0a3060dd8208dc9dbf4083ccd
lsdosar <61499892+lsdosar@users.noreply.github.com>
2023-02-21 11:33:45 +0200

    Changed logging definition

--- lib/util.py
+++ lib/util.py
@@ -22,20 +22,20 @@ def get_date_string(date_object):
 # Configuring the logging handler.
 logFormatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
 rootLogger = logging.getLogger()
-fileName = get_date_string(datetime.now())+'sonarqube_exporter'
-logPath = 'logs'
-fileHandler = logging.FileHandler("{0}/{1}.log".format(logPath, fileName))
-fileHandler.setFormatter(logFormatter)
+#fileName = get_date_string(datetime.now())+'sonarqube_exporter'
+#logPath = 'logs'
+#fileHandler = logging.FileHandler("{0}/{1}.log".format(logPath, fileName))
+#fileHandler.setFormatter(logFormatter)
 
 # To avoid duplicated logs.
 if (rootLogger.hasHandlers()):
     rootLogger.handlers.clear()
-rootLogger.addHandler(fileHandler)
+#rootLogger.addHandler(fileHandler)
 
 consoleHandler = logging.StreamHandler(sys.stdout)
 consoleHandler.setFormatter(logFormatter)
 rootLogger.addHandler(consoleHandler)
-logging.getLogger().setLevel(logging.DEBUG)
+logging.getLogger().setLevel(logging.INFO)
 logger = logging.getLogger()
 
 # Convert sr to json
